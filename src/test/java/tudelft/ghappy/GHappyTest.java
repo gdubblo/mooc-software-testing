606d626befe028cde15972d73b75673bb7d759df
Cristina <crismv1003@protonmail.com>
2018-11-16 21:58:24 -0800

    Committing gHappyTest class

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,36 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+        @Test
+    public void gHappyTest1() {
+        GHappy solution = new GHappy();
+        Boolean result  = solution.gHappy("xxggxx");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void gHappyTest2() {
+        GHappy solution = new GHappy();
+        Boolean result  = solution.gHappy("xxgxx");
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void gHappyTest3() {
+        GHappy solution = new GHappy();
+        Boolean result  = solution.gHappy("xxggyygxx");
+        Assertions.assertEquals(false, result);
+    }
+
+    @Test
+    public void gHappyTest4() {
+        GHappy solution = new GHappy();
+        Boolean result  = solution.gHappy("ggyygg");
+        Assertions.assertEquals(true, result);
+    }
+
+
 }
