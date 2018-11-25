1bd820995d9c0823f2e173c14bc23c8672dd5e7b
Cristina <crismv1003@protonmail.com>
2018-11-25 13:54:12 -0800

    pushing test implementation

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,28 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CaesarShiftCipherTest {
+
+    @Test
+    public void shiftByThreeEnd() {
+        CaesarShiftCipher test1 = new CaesarShiftCipher();
+        String result = test1.CaesarShiftCipher("xyz", 3);
+        Assertions.assertEquals("abc", result);
+    }
+
+    @Test
+    public void shiftByThreeStart() {
+        CaesarShiftCipher test1 = new CaesarShiftCipher();
+        String result = test1.CaesarShiftCipher("abc", 3);
+        Assertions.assertEquals("def", result);
+    }
+
+    @Test
+    public void shiftByZero() {
+        CaesarShiftCipher test2 = new CaesarShiftCipher();
+        String result = test2.CaesarShiftCipher("abc", 0);
+        Assertions.assertEquals("abc", result); }
 
 }
